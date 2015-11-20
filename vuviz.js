8cf612cdf71008138a541cec62d861b84ca190f1
Yann <yann.leherve@yahoo.fr>
2015-11-20 18:07:30 +0100

    Titre prephib+commentaire

--- vuviz.js
+++ vuviz.js
@@ -1,7 +1,7 @@
 angular.module('vuvizApp', [])
   .controller('VuvizController', function() {
     var control = this;
-    control.nomApplication = "VuViz";
+    control.nomApplication = "Prephib";
     control.indices = [
       {nom:'DOW JONES', sectoriel:false, selected: false},
       {nom:'CAC40', sectoriel:false, selected:false}];
