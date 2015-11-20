799d4c1b647ff7a9a1456c6af29be88871d0d128
Yann <yann.leherve@yahoo.fr>
2015-11-20 18:23:32 +0100

    Liste indice
    Ajout de la liste des indice plus le continent correspondant (j'espere
    que je fait pas de betuise)

--- vuviz.js
+++ vuviz.js
@@ -3,6 +3,19 @@ angular.module('vuvizApp', [])
     var control = this;
     control.nomApplication = "Prephib";
     control.indices = [
-      {nom:'DOW JONES', sectoriel:false, selected: false},
-      {nom:'CAC40', sectoriel:false, selected:false}];
+		//Etat Unis
+      {nom:'DOW JONES', continent:'Etat-Unis', sectoriel:false, selected: false},
+      {nom:'NASDAQ', continent:'Etat-Unis', sectoriel:false, selected:false}];
+      {nom:'S&P500', continent:'Etat-Unis', sectoriel:false, selected:false}];
+	  //europe
+      {nom:'DAX', continent:'Europe', sectoriel:false, selected:false}];
+      {nom:'FOOTSIE', continent:'Europe', sectoriel:false, selected:false}];
+      {nom:'CAC40', continent:'Europe', sectoriel:false, selected:false}];
+      {nom:'IBEX', continent:'Europe', sectoriel:false, selected:false}];
+      {nom:'MIB', continent:'Europe', sectoriel:false, selected:false}];
+      {nom:'IBEX', continent:'Europe', sectoriel:false, selected:false}];
+	  //ASIE
+      {nom:'NIKKEI250', continent:'Asie', sectoriel:false, selected:false}];
+      {nom:'SSE', continent:'Asie', sectoriel:false, selected:false}];
+      {nom:'HS', continent:'Asie', sectoriel:false, selected:false}];
 });
