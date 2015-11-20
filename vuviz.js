51995f7f3609c3141650678beed4062281c034b5
Ophir LOJKINE <pere.jobs@gmail.com>
2015-11-21 02:12:30 +0300

    Câblage angular
    Affichage dynamique de la liste des indices dans les vignettes, et mise
    en fonctionnement du choix d'indice par type et par continent.

--- vuviz.js
+++ vuviz.js
@@ -4,9 +4,9 @@ angular.module('vuvizApp', [])
     control.nomApplication = "Prephib";
     control.indices = [
 		//Etat Unis
-      {nom:'DOW JONES', continent:'Etat-Unis', sectoriel:false, selected: false},
-      {nom:'NASDAQ', continent:'Etat-Unis', sectoriel:false, selected:false},
-      {nom:'S&P500', continent:'Etat-Unis', sectoriel:false, selected:false},
+      {nom:'DOW JONES', continent:'État-Unis', sectoriel:false, selected: false},
+      {nom:'NASDAQ', continent:'État-Unis', sectoriel:false, selected:false},
+      {nom:'S&P500', continent:'État-Unis', sectoriel:false, selected:false},
 	  //europe
       {nom:'DAX', continent:'Europe', sectoriel:false, selected:false},
       {nom:'FOOTSIE', continent:'Europe', sectoriel:false, selected:false},
@@ -18,4 +18,29 @@ angular.module('vuvizApp', [])
       {nom:'NIKKEI250', continent:'Asie', sectoriel:false, selected:false},
       {nom:'SSE', continent:'Asie', sectoriel:false, selected:false},
       {nom:'HS', continent:'Asie', sectoriel:false, selected:false}];
+
+    control.aff_sectoriel = false;
+    control.aff_continent = "Europe";
+
+    control.continents = function() {
+      // Retourne la liste des continents
+      return Object.keys(control.indices.reduce(function(continents, indice){
+        continents[indice.continent] = true;
+        return continents;
+      }, {}));
+    };
+
+    control.aff_indices = function aff_indices() {
+      // Retourne la liste de tous les indices qui correspindent aux critères de recherche
+      return control.indices.filter(function(i){
+        return i.continent === control.aff_continent && i.sectoriel === control.aff_sectoriel;
+      });
+    };
+
+    control.indices_selec = function() {
+      // Retourne la liste des indices cochés
+      return control.indices.filter(function (i) {
+        return i.selected;
+      });
+    }
 });
