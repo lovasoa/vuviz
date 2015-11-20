1f05a553da01e46f364a67172c19faf8c59b6cc9
Ophir LOJKINE <pere.jobs@gmail.com>
2015-11-20 20:45:43 +0300

    Correction erreur de syntaxe

--- vuviz.js
+++ vuviz.js
@@ -5,17 +5,17 @@ angular.module('vuvizApp', [])
     control.indices = [
 		//Etat Unis
       {nom:'DOW JONES', continent:'Etat-Unis', sectoriel:false, selected: false},
-      {nom:'NASDAQ', continent:'Etat-Unis', sectoriel:false, selected:false}];
-      {nom:'S&P500', continent:'Etat-Unis', sectoriel:false, selected:false}];
+      {nom:'NASDAQ', continent:'Etat-Unis', sectoriel:false, selected:false},
+      {nom:'S&P500', continent:'Etat-Unis', sectoriel:false, selected:false},
 	  //europe
-      {nom:'DAX', continent:'Europe', sectoriel:false, selected:false}];
-      {nom:'FOOTSIE', continent:'Europe', sectoriel:false, selected:false}];
-      {nom:'CAC40', continent:'Europe', sectoriel:false, selected:false}];
-      {nom:'IBEX', continent:'Europe', sectoriel:false, selected:false}];
-      {nom:'MIB', continent:'Europe', sectoriel:false, selected:false}];
-      {nom:'IBEX', continent:'Europe', sectoriel:false, selected:false}];
+      {nom:'DAX', continent:'Europe', sectoriel:false, selected:false},
+      {nom:'FOOTSIE', continent:'Europe', sectoriel:false, selected:false},
+      {nom:'CAC40', continent:'Europe', sectoriel:false, selected:false},
+      {nom:'IBEX', continent:'Europe', sectoriel:false, selected:false},
+      {nom:'MIB', continent:'Europe', sectoriel:false, selected:false},
+      {nom:'IBEX', continent:'Europe', sectoriel:false, selected:false},
 	  //ASIE
-      {nom:'NIKKEI250', continent:'Asie', sectoriel:false, selected:false}];
-      {nom:'SSE', continent:'Asie', sectoriel:false, selected:false}];
+      {nom:'NIKKEI250', continent:'Asie', sectoriel:false, selected:false},
+      {nom:'SSE', continent:'Asie', sectoriel:false, selected:false},
       {nom:'HS', continent:'Asie', sectoriel:false, selected:false}];
 });
