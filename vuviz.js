f039f7e400f43ae20fcfb26bae2b6286896a8ed4
Ophir LOJKINE <pere.jobs@gmail.com>
2015-12-01 17:27:21 +0300

    Utilisation de filtres angular au lieu de fonctions custom
    Cela résoud un bug, et améliore les performances.
    Le bug: https://docs.angularjs.org/error/$rootScope/infdig?p0=10&p1=[[{%22msg%22:%22fn:%20regularInterceptedExpression%22,%22newVal%22:[],%22oldVal%22:[]}],[{%22msg%22:%22fn:%20regularInterceptedExpression%22,%22newVal%22:[],%22oldVal%22:%22...%22}],[{%22msg%22:%22fn:%20regularInterceptedExpression%22,%22newVal%22:[],%22oldVal%22:%22...%22}],[{%22msg%22:%22fn:%20regularInterceptedExpression%22,%22newVal%22:[],%22oldVal%22:%22...%22}],[{%22msg%22:%22fn:%20regularInterceptedExpression%22,%22newVal%22:[],%22oldVal%22:%22...%22}]]

--- vuviz.js
+++ vuviz.js
@@ -1,8 +1,9 @@
-angular.module('vuvizApp', ["nvd3"])
-  .controller('VuvizController', function() {
+var app = angular.module('vuvizApp', ["nvd3"]);
+
+app.controller('VuvizController', function($scope, $filter) {
     var control = this;
-    control.nomApplication = "Prévision Financière et Boursière";
-    control.indices = [
+    $scope.nomApplication = "Prévision Financière et Boursière";
+    $scope.indices = [
 		//Etat Unis
       {nom:'DOW JONES', continent:'État-Unis', sectoriel:false, selected: false},
       {nom:'NASDAQ', continent:'État-Unis', sectoriel:false, selected:false},
@@ -19,46 +20,39 @@ angular.module('vuvizApp', ["nvd3"])
       {nom:'SSE', continent:'Asie', sectoriel:false, selected:false},
       {nom:'HS', continent:'Asie', sectoriel:false, selected:false}];
 
-    control.aff_sectoriel = false;
-    control.aff_continent = "Europe";
+    $scope.liste_continents = ["État-Unis", "Europe", "Asie"];
 
-    control.graph1 = {
+    $scope.filtre = {
+      "continent" : "Europe",
+      "sectoriel" : false
+    };
+
+    $scope.selected = {selected: true};
+
+    $scope.graph1 = {
       "options" : {
         chart: {
            type: 'lineChart',
            height: 450,
         }
-      },
-      "donnees" : function() {
-        return control.indices_selec().map(function(i) {
-            return {
-              values : [{x:0,y:i.nom.charCodeAt(0)}, {x:1,y:i.nom.charCodeAt(1)}, {x:2,y:80}],
-              key: i.nom,
-              color: "#00"+i.nom.charCodeAt(0)+i.nom.charCodeAt(1)
-            };
-        });
       }
     };
+});
 
-    control.continents = function() {
-      // Retourne la liste des continents
-      return Object.keys(control.indices.reduce(function(continents, indice){
-        continents[indice.continent] = true;
-        return continents;
-      }, {}));
-    };
-
-    control.aff_indices = function aff_indices() {
-      // Retourne la liste de tous les indices qui correspindent aux critères de recherche
-      return control.indices.filter(function(i){
-        return i.continent === control.aff_continent && i.sectoriel === control.aff_sectoriel;
-      });
-    };
-
-    control.indices_selec = function() {
-      // Retourne la liste des indices cochés
-      return control.indices.filter(function (i) {
-        return i.selected;
-      });
+app.filter('valeursIndices', function() {
+  // Filtre qui prend en entrée un tableau d'indices et sort un tableau de
+  // valeurs d'indices compatible avec nvd3
+  var _cache = [];
+  return function(indices) {
+    _cache.length = indices.length;
+    for(var j=0; j<indices.length; j++) {
+      var i = indices[j];
+      _cache[j] = {
+        values : [{x:0,y:i.nom.charCodeAt(0)}, {x:1,y:i.nom.charCodeAt(1)}, {x:2,y:80}],
+        key: i.nom,
+        color: "#00"+i.nom.charCodeAt(0)+i.nom.charCodeAt(1)
+      }
     }
+    return _cache;
+  };
 });
