00f6108a6e1cdaf05aef2fafe74eeea7ed1da983
Ophir LOJKINE <pere.jobs@gmail.com>
2016-02-11 13:04:59 +0300

    Ajout id indice dans l'API

--- api/indices.php
+++ api/indices.php
@@ -3,13 +3,14 @@ header("Content-Type: text/json; charset=utf8");
 require_once("./connect.php");
 $sql = "
 SELECT
-  nom_indice, nom_continent, sectoriel, couleur
+  id_indice, nom_indice, nom_continent, sectoriel, couleur
 FROM indice
 INNER JOIN continent ON continent.id_continent = indice.id_continent;
 ";
 $res = array();
 foreach ($BDD->query($sql) as $row) {
   $res[] = array(
+    "id"        => intval($row['id_indice']),
     "nom"       => $row['nom_indice'],
     "continent" => $row['nom_continent'],
     "sectoriel" => boolval($row['sectoriel']),
