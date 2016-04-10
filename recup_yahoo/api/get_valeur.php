aa39d950f10699552f9fb0caa6cdb97c10999215
Yann <yann.leherve@yahoo.fr>
2016-04-10 21:00:44 +0200

    Changement de la fonction de récup des données
    Avoir les données les plus récente pour chaque indice

--- recup_yahoo/api/get_valeur.php
+++ recup_yahoo/api/get_valeur.php
@@ -27,17 +27,29 @@ $stmt = $BDD->prepare("
 SELECT
 	indice.nom_indice,
 	type_valeur_api.nom_type_valeur,
-	valeur,
-	max(date) as date,
+	t1.valeur,
+	t1.date,
 	(CASE type_valeur_api.couleur_type_valeur
 			 WHEN '' THEN indice.couleur
 			 ELSE type_valeur_api.couleur_type_valeur
 	END) AS couleur
-FROM `valeur_api`
-INNER JOIN type_valeur_api ON type_valeur_api.id_type_valeur_api = valeur_api.id_type_valeur_api
-INNER JOIN indice ON valeur_api.id_indice = indice.id_indice
-GROUP BY valeur_api.id_indice, valeur_api.id_type_valeur_api
-ORDER BY valeur_api.id_indice, valeur_api.date, valeur_api.id_type_valeur_api
+FROM `valeur_api` as t1
+INNER JOIN
+(
+	SELECT t2.id_indice,
+	t2.id_type_valeur_api,
+	t2.valeur,
+	max(t2.date) AS date_max
+	FROM `valeur_api` as t2
+	GROUP BY t2.id_indice, t2.id_type_valeur_api
+) tmp 
+	ON tmp.id_indice = t1.id_indice
+	AND tmp.id_type_valeur_api = t1.id_type_valeur_api
+	AND tmp.date_max = t1.date
+    
+INNER JOIN type_valeur_api ON type_valeur_api.id_type_valeur_api = t1.id_type_valeur_api
+INNER JOIN indice ON t1.id_indice = indice.id_indice
+GROUP BY t1.id_indice, t1.id_type_valeur_api
 
 ");
 
